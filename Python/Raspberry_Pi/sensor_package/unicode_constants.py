c1e2727f762b9eca520828d577ce1b66fa82a025
amorphousWaste <20346603+amorphousWaste@users.noreply.github.com>
2021-07-10 01:23:40 -0700

    Make a class for special symbols

---
+++ Python/Raspberry_Pi/sensor_package/unicode_constants.py
@@ -0,0 +1,16 @@
+"""Unicode values."""
+
+
+class UnicodeConstants(object):
+    """Constants."""
+
+    def __init__(self):
+        """Init."""
+        super(UnicodeConstants, self).__init__()
+
+        # Unicode symbol strings
+        self.celsius = '\u00B0C'
+        self.fahrenheit = '\u00B0F'
+        self.ohm = '\u03A9'
+        self.sds011_conc = '\u00B5g/m\u00B3'  # Micrograms per cubic meter
+        self.subscript_2 = '\u2082'
