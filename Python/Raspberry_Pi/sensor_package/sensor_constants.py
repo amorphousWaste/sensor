faff0ac112c2af615a7e2eb373bc748c851e0541
amorphousWaste <20346603+amorphousWaste@users.noreply.github.com>
2021-07-10 01:24:58 -0700

    Change sensor constants to be a callable class instead of a flat file
    This allows for querying, instantiating, and referencing values without 
    needing to create a large number of globals.

--- Python/Raspberry_Pi/sensor_package/sensor_constants.py
+++ Python/Raspberry_Pi/sensor_package/sensor_constants.py
@@ -1,141 +1,155 @@
-#  sensor_constants.py
+"""Sensor Constants.
 
-#  This file defines constant values which are used in the control 
-#  of the Metriful MS430 board and the interpretation of its output data.
-#  All values have been taken from the MS430 datasheet.
+This file defines constant values which are used in the control
+of the Metriful MS430 board and the interpretation of its output data.
+All values have been taken from the MS430 datasheet.
 
-#  Copyright 2020 Metriful Ltd. 
-#  Licensed under the MIT License - for further details see LICENSE.txt
+Copyright 2020 Metriful Ltd.
+Licensed under the MIT License - for further details see LICENSE.txt
 
-#  For code examples, datasheet and user guide, visit 
-#  https://github.com/metriful/sensor
+For code examples, datasheet and user guide, visit
+https://github.com/metriful/sensor
+"""
 
-# Settings registers
-PARTICLE_SENSOR_SELECT_REG = 0x07 
-LIGHT_INTERRUPT_ENABLE_REG = 0x81     
-LIGHT_INTERRUPT_THRESHOLD_REG = 0x82  
-LIGHT_INTERRUPT_TYPE_REG = 0x83       
-LIGHT_INTERRUPT_POLARITY_REG = 0x84   
-SOUND_INTERRUPT_ENABLE_REG = 0x85     
-SOUND_INTERRUPT_THRESHOLD_REG = 0x86  
-SOUND_INTERRUPT_TYPE_REG = 0x87       
-CYCLE_TIME_PERIOD_REG = 0x89 
 
-# Executable commands 
-ON_DEMAND_MEASURE_CMD = 0xE1
-RESET_CMD = 0xE2
-CYCLE_MODE_CMD = 0xE4
-STANDBY_MODE_CMD = 0xE5
-LIGHT_INTERRUPT_CLR_CMD = 0xE6
-SOUND_INTERRUPT_CLR_CMD = 0xE7
+class SensorConstants(object):
+    """Constants."""
 
-# Read the operational mode
-OP_MODE_READ = 0x8A         
+    def __init__(self):
+        """Init."""
+        super(SensorConstants, self).__init__()
 
-# Read data for whole categories 
-AIR_DATA_READ = 0x10
-AIR_QUALITY_DATA_READ = 0x11
-LIGHT_DATA_READ = 0x12
-SOUND_DATA_READ = 0x13
-PARTICLE_DATA_READ = 0x14
+        # Settings registers
+        self.particle_sensor_select_reg = 0x07
+        self.light_interrupt_enable_reg = 0x81
+        self.light_interrupt_threshold_reg = 0x82
+        self.light_interrupt_type_reg = 0x83
+        self.light_interrupt_polarity_reg = 0x84
+        self.sound_interrupt_enable_reg = 0x85
+        self.sound_interrupt_threshold_reg = 0x86
+        self.sound_interrupt_type_reg = 0x87
+        self.cycle_time_period_reg = 0x89
 
-# Read individual data quantities
-T_READ = 0x21
-P_READ = 0x22
-H_READ = 0x23
-G_READ = 0x24
-AQI_READ = 0x25
-CO2E_READ = 0x26
-BVOC_READ = 0x27
-AQI_ACCURACY_READ = 0x28
-ILLUMINANCE_READ = 0x31
-WHITE_LIGHT_READ = 0x32
-SPL_READ = 0x41
-SPL_BANDS_READ = 0x42
-SOUND_PEAK_READ = 0x43
-SOUND_STABLE_READ = 0x44
-DUTY_CYCLE_READ = 0x51
-CONCENTRATION_READ = 0x52
-PARTICLE_VALID_READ = 0x53
+        # Executable commands
+        self.on_demand_measure_cmd = 0xE1
+        self.reset_cmd = 0xE2
+        self.cycle_mode_cmd = 0xE4
+        self.standby_mode_cmd = 0xE5
+        self.light_interrupt_clr_cmd = 0xE6
+        self.sound_interrupt_clr_cmd = 0xE7
 
-###############################################################
+        # Read the operational mode
+        self.op_mode_read = 0x8A
 
-# I2C address of sensor board: can select using solder bridge
-I2C_ADDR_7BIT_SB_OPEN = 0x71   # if solder bridge is left open
-I2C_ADDR_7BIT_SB_CLOSED = 0x70 # if solder bridge is soldered closed
+        # Read data for whole categories
+        self.air_data_read = 0x10
+        self.air_quality_data_read = 0x11
+        self.light_data_read = 0x12
+        self.sound_data_read = 0x13
+        self.particle_data_read = 0x14
 
-# Values for enabling/disabling of sensor functions
-ENABLED = 1
-DISABLED = 0
+        # Read individual data quantities
+        self.t_read = 0x21
+        self.p_read = 0x22
+        self.h_read = 0x23
+        self.g_read = 0x24
+        self.aqi_read = 0x25
+        self.co2e_read = 0x26
+        self.bvoc_read = 0x27
+        self.aqi_accuracy_read = 0x28
+        self.illuminance_read = 0x31
+        self.white_light_read = 0x32
+        self.spl_read = 0x41
+        self.spl_bands_read = 0x42
+        self.sound_peak_read = 0x43
+        self.sound_stable_read = 0x44
+        self.duty_cycle_read = 0x51
+        self.concentration_read = 0x52
+        self.particle_valid_read = 0x53
 
-# Device modes
-STANDBY_MODE = 0
-CYCLE_MODE = 1
+        # I2C address of sensor board: can select using solder bridge
+        # If solder bridge is left open
+        self.i2c_addr_7bit_sb_open = 0x71
+        # If solder bridge is soldered closed
+        self.i2c_addr_7bit_sb_closed = 0x70
+        # NOTE: I don't know what the point of this value being duplicated is
+        self.i2c_7bit_address = self.i2c_addr_7bit_sb_open
 
-LIGHT_INTERRUPT_THRESHOLD_BYTES = 3
-SOUND_INTERRUPT_THRESHOLD_BYTES = 2
+        # Values for enabling/disabling of sensor functions
+        self.enabled = 1
+        self.disabled = 0
 
-# Frequency bands for sound level measurement
-SOUND_FREQ_BANDS = 6
-sound_band_mids_Hz = [125, 250, 500, 1000, 2000, 4000]
-sound_band_edges_Hz = [88, 177, 354, 707, 1414, 2828, 5657]
+        # Device modes
+        self.standby_mode = 0
+        self.cycle_mode = 1
 
-# Cycle mode time period
-CYCLE_PERIOD_3_S = 0
-CYCLE_PERIOD_100_S = 1
-CYCLE_PERIOD_300_S = 2
+        self.light_interrupt_threshold_bytes = 3
+        self.sound_interrupt_threshold_bytes = 2
 
-# Sound interrupt type
-SOUND_INT_TYPE_LATCH = 0
-SOUND_INT_TYPE_COMP = 1
+        # Frequency bands for sound level measurement
+        self.sound_freq_bands = 6
+        self.sound_band_mids_hz = [125, 250, 500, 1000, 2000, 4000]
+        self.sound_band_edges_hz = [88, 177, 354, 707, 1414, 2828, 5657]
 
-# Maximum for illuminance measurement and threshold setting
-MAX_LUX_VALUE = 3774 
+        # Cycle mode time period
+        self.cycle_period_3_s = 0
+        self.cycle_period_100_s = 1
+        self.cycle_period_300_s = 2
 
-# Light interrupt type
-LIGHT_INT_TYPE_LATCH = 0
-LIGHT_INT_TYPE_COMP = 1
+        # Sound interrupt type
+        self.sound_int_type_latch = 0
+        self.sound_int_type_comp = 1
 
-# Light interrupt polarity
-LIGHT_INT_POL_POSITIVE = 0
-LIGHT_INT_POL_NEGATIVE = 1
+        # Maximum for illuminance measurement and threshold setting
+        self.max_lux_value = 3774
 
-# Decoding the temperature integer.fraction value format
-TEMPERATURE_VALUE_MASK = 0x7F 
-TEMPERATURE_SIGN_MASK = 0x80 
+        # Light interrupt type
+        self.light_int_type_latch = 0
+        self.light_int_type_comp = 1
 
-# Particle sensor module selection:
-PARTICLE_SENSOR_OFF = 0
-PARTICLE_SENSOR_PPD42 = 1
-PARTICLE_SENSOR_SDS011 = 2
+        # Light interrupt polarity
+        self.light_int_pol_positive = 0
+        self.light_int_pol_negative = 1
 
-###############################################################
+        # Decoding the temperature integer.fraction value format
+        self.temperature_value_mask = 0x7F
+        self.temperature_sign_mask = 0x80
 
-# Byte lengths for each readable data quantity and data category
+        # Particle sensor module selection
+        self.particle_sensor_off = 0
+        self.particle_sensor_ppd42 = 1
+        self.particle_sensor_sds011 = 2
 
-T_BYTES = 2
-P_BYTES = 4
-H_BYTES = 2
-G_BYTES = 4
-AIR_DATA_BYTES = 12
+        # Byte lengths for each readable data quantity and data category
+        self.t_bytes = 2
+        self.p_bytes = 4
+        self.h_bytes = 2
+        self.g_bytes = 4
+        self.air_data_bytes = 12
 
-AQI_BYTES = 3
-CO2E_BYTES = 3
-BVOC_BYTES = 3
-AQI_ACCURACY_BYTES = 1
-AIR_QUALITY_DATA_BYTES = 10
+        self.aqi_bytes = 3
+        self.co2e_bytes = 3
+        self.bvoc_bytes = 3
+        self.aqi_accuracy_bytes = 1
+        self.air_quality_data_bytes = 10
 
-ILLUMINANCE_BYTES = 3
-WHITE_BYTES = 2
-LIGHT_DATA_BYTES = 5
+        self.illuminance_bytes = 3
+        self.white_bytes = 2
+        self.light_data_bytes = 5
 
-SPL_BYTES = 2
-SPL_BANDS_BYTES = (2*SOUND_FREQ_BANDS)
-SOUND_PEAK_BYTES = 3
-SOUND_STABLE_BYTES = 1
-SOUND_DATA_BYTES = 18
+        self.spl_bytes = 2
+        self.spl_bands_bytes = (2 * self.sound_freq_bands)
+        self.sound_peak_bytes = 3
+        self.sound_stable_bytes = 1
+        self.sound_data_bytes = 18
 
-DUTY_CYCLE_BYTES = 2
-CONCENTRATION_BYTES = 3
-PARTICLE_VALID_BYTES = 1
-PARTICLE_DATA_BYTES = 6
+        self.duty_cycle_bytes = 2
+        self.concentration_bytes = 3
+        self.particle_valid_bytes = 1
+        self.particle_data_bytes = 6
+
+        # GPIO (input/output) header pin numbers.
+        # These must match the hardware wiring.
+        self.light_int_pin = 7  # Raspberry Pi pin 7 connects to LIT
+        self.sound_int_pin = 8  # Raspberry Pi pin 8 connects to SIT
+        self.ready_pin = 11  # Raspberry Pi pin 11 connects to RDY
